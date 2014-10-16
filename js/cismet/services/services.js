bdcf68413911cada81ab1ec2fd14b1d6fa367988
Daniel Meiers <daniel.meiers@cismet.de>
2014-10-16 16:31:51 +0200

    #10 new service for worldstate augmentation
    the ref app uses the concept of rendering descriptors to allow a easy
    configurable visualisation of worldsates. Currently the structure of
    those rendering descriptors are under ungoing changes. Thus a service is
    used to augment worldstates with the correct rendering descriptor
    format.

--- js/cismet/services/services.js
+++ js/cismet/services/services.js
@@ -3,16 +3,6 @@ angular.module('de.cismet.smartAdmin.services',
         'ngResource',
         'de.cismet.custom.services'
     ]
-    ).config(
-    [
-        '$provide',
-        function ($provide) {
-            'use strict';
-            $provide.constant('DEBUG', 'true');
-            $provide.constant('CRISMA_DOMAIN', 'CRISMA');
-            $provide.constant('CRISMA_ICMM_API', 'http://crisma.cismet.de/refApp/icmm_api');
-        }
-    ]
     ).service('ShortCutService',
     [
         'LayoutService',
@@ -96,7 +86,7 @@ angular.module('de.cismet.smartAdmin.services',
         }
     ]
     ).service(
-    'CommonWorldstateUtils',
+    'Utils',
     [
         '$q',
         function ($q) {
@@ -113,6 +103,127 @@ angular.module('de.cismet.smartAdmin.services',
                 wsPath.push(ws);
                 return wsPath.reverse();
 //                return defer.promise;
+            };
+
+            serviceObj.augmentWorldstateDataWithRenderingDescriptors = function (worldstate) {
+                var renderingDescriptorPrototype = {
+                    gridWidth: 'col-sm-6',
+                    priority: 1,
+                    displayHeader: true,
+                    sortable: true,
+                    fullSize: true,
+                    collapsed: false,
+                    collapsible: true,
+                    bodyDirective: 'wms-leaflet',
+                    padding: 'no-padding'
+                }, dataslot, i, tempRd;
+                if (worldstate.worldstatedata) {
+                    for (i = 0; i < worldstate.worldstatedata.length; i++) {
+                        dataslot = worldstate.worldstatedata[i];
+                        if(dataslot){
+                            tempRd = Object.create(renderingDescriptorPrototype);
+                            tempRd.title = dataslot.name;
+                            tempRd.priority = i + 2;
+                            dataslot.renderingdescriptor = [];
+                            dataslot.renderingdescriptor.push(tempRd);
+                        }
+                    }
+                }
+                return worldstate;
+            };
+            
+            serviceObj.augmentIccDataWithRenderingDescriptors = function (worldstate) {
+                var rd;
+                rd = [
+                    {
+                        gridWidth: 'col-sm-12',
+                        priority: 1,
+                        displayHeader: true,
+                        sortable: false,
+                        fullSize: true,
+                        collapsed: false,
+                        collapsible: true,
+                        bodyDirective: 'icc-data-body',
+                        headerDirective: 'icc-data-header',
+                        colourClasses: [
+                            'panel-purple',
+                            'panel-orange',
+                            'panel-greenLight',
+                            'panel-blue',
+                            'panel-redLight'
+                        ],
+                        mergeId: 'iccIndicatorCriteriaWidget',
+                        title: 'Indicator & Criteria'
+                    },
+                    {
+                        gridWidth: 'col-sm-12',
+                        priority: 0,
+                        displayHeader: true,
+                        sortable: false,
+                        fullSize: true,
+                        collapsed: false,
+                        collapsible: true,
+                        bodyDirective: 'mini-indicator-body',
+                        colourClasses: [
+                            'txt-color-blue',
+                            'txt-color-purple',
+                            'txt-color-greenDark',
+                            'txt-color-orange',
+                            'txt-color-redLight'
+                        ],
+                        widgetArea: false
+                    }
+                ];
+                
+                if(worldstate && worldstate.iccdata){
+                    var i, iccItem;
+                    for(i=0;i<worldstate.iccdata.length;i++){
+                        iccItem =worldstate.iccdata[i];
+                        if(iccItem){
+                            iccItem.renderingdescriptor = Object.create(rd);
+                        }
+                    }
+                }
+                
+                return worldstate;
+            };
+            serviceObj.mergeWMSLayer = function (worldstate) {
+                var osmMergeId = 'osmHelperLayer';
+                var regionMergeId = 'regionHelperLayer';
+                var backgroundMergeId = 'backgroundHelperLayer';
+                var i, renderingdescriptor, dataslot;
+                if (worldstate.worldstatedata) {
+
+                    for (i = 0; i < worldstate.worldstatedata.length; i++) {
+                        renderingdescriptor = worldstate.worldstatedata[i].renderingdescriptor[0];
+                        dataslot = worldstate.worldstatedata[i];
+                        switch (dataslot.name) {
+                            case 'planet_osm_polygon':
+                            case 'ortho':
+                            case 'planet_osm_line':
+                            case 'planet_osm_point':
+                                renderingdescriptor.mergeId = osmMergeId;
+                                renderingdescriptor.priority = 20;
+                                break;
+                            case 'regione':
+                            case 'province':
+                            case 'comune_aq':
+                            case 'hospitals':
+                            case 'schools':
+                            case 'power_towers':
+                                renderingdescriptor.mergeId = regionMergeId;
+                                renderingdescriptor.priority = 30;
+                                break;
+                            case 'contour_dem_25':
+                            case 'zones':
+                            case 'clc2006':
+                                renderingdescriptor.mergeId = backgroundMergeId;
+                                renderingdescriptor.priority = 40;
+                                break;
+                        }
+                    }
+                }
+                return worldstate;
             };
 
             return serviceObj;
