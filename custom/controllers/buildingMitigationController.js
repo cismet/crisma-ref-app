01a4c7a51f47927038a2dd94f778e09d1f367165
Martin Scholl <martin.scholl@cismet.de>
2015-02-27 12:05:49 +0100

    [issue #15] budget and building improvement step according to spec

--- custom/controllers/buildingMitigationController.js
+++ custom/controllers/buildingMitigationController.js
@@ -11,6 +11,7 @@ angular.module(
 
             $scope.wizard = {};
             $scope.wizard.currentStep = '';
+            // TODO: proper validation, this should be false
             $scope.wizard.canProceed = true;
             $scope.wizard.isFinishStep = function () {
                 return $scope.wizard.currentStep === 'Parameterize Earthquake';
@@ -27,6 +28,7 @@ angular.module(
                     return true;
                 }
 
+                // TODO: proper validation, this should be false
                 return true;
             };
 
@@ -34,8 +36,35 @@ angular.module(
             
             $scope.params.improvements = {};
             $scope.params.improvements.timeframe = 50;
-            $scope.budget = {};
-            $scope.budget.timeframeOptions = [{"value": 50, "label": '<i class="fa fa-calendar"/>&nbsp;50 Years'}];
+            $scope.params.improvements.availableBudget = 0;
+            $scope.params.improvements.interestRate = 0;
+            $scope.params.improvements.aToB = 0;
+            $scope.params.improvements.aToC = 0;
+            $scope.params.improvements.aToD = 0;
+            $scope.params.improvements.bToC = 0;
+            $scope.params.improvements.bToD = 0;
+            $scope.params.improvements.cToD = 0;
+            
+            $scope.params.energy = {};
+            $scope.params.energy.aToD = {};
+            $scope.params.energy.aToD.level = 1;
+            $scope.params.energy.aToD.percentage = 0;
+            $scope.params.energy.bToD = {};
+            $scope.params.energy.bToD.level = 1;
+            $scope.params.energy.bToD.percentage = 0;
+            $scope.params.energy.cToD = {};
+            $scope.params.energy.cToD.level = 1;
+            $scope.params.energy.cToD.percentage = 0;
+            $scope.params.energy.termalKWhPrice = 0;
+            
+            $scope.params.budget = {};
+            $scope.params.budget.timeframeOptions = [{"value": 50, "label": '<i class="fa fa-calendar"/>&nbsp;50 Years'}];
+            $scope.params.budget.government = 0;
+            $scope.params.budget.citizens = 0;
+            
+            $scope.params.tax = {};
+            $scope.params.tax.energy = 0;
+            $scope.params.tax.seismic = 0;
 
             $scope.$watch('wizard.currentStep', function (n) {
                 if (n) {
@@ -61,6 +90,7 @@ angular.module(
                     $scope.wizard.canProceed =
                         ($scope.wizard.validators[$scope.wizard.currentStep] || $scope.wizard.validators.noVal)();
                 } else {
+                    // TODO: proper validation, this should be false instead
                     $scope.wizard.canProceed = true;
                 }
 
