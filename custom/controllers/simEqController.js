f397051b4466af5abe4fdf6719917b4902aafe35
Martin Scholl <martin.scholl@cismet.de>
2015-04-21 17:17:28 +0200

    [issue #17] initial qnd impl for supporting up to 5 eq rounds

--- custom/controllers/simEqController.js
+++ custom/controllers/simEqController.js
@@ -4,24 +4,92 @@ angular.module(
     'de.cismet.custom.controllers.simEqController', 
     [
         '$scope',
-        '$timeout',
         'leafletData',
-        function ($scope, $timeout, leafletData) {
+        function ($scope, leafletData) {
             'use strict';
 
             var epiCenterIcon, marker, setEpicenter;
+
+            $scope.wizard = {};
+            $scope.wizard.currentStep = '';
+            // TODO: proper validation, this should be false
+            $scope.wizard.canProceed = true;
+            $scope.wizard.isFinishStep = function () {
+                return $scope.wizard.currentStep.indexOf($scope.params.tdvRounds + '') > 0;
+            };
+            $scope.wizard.finish = function () {
+//                $scope.params.run = true;
+                $scope.$hide();
+            };
+            $scope.wizard.proceedButtonText = 'Next';
             
             $scope.center = {lat: 42.355244, lng: 13.393662, zoom: 12};
             
-            $scope.params.useShakemap = false;
-            $scope.params.applyTDV = false;
-            $scope.params.latitude = 42.355244;
-            $scope.params.longitude = 13.393662;
-            $scope.params.depth = 0;
-            $scope.params.magnitude = 0;
+            $scope.paramArray = [];
+            $scope.defaultParams = {
+                useShakemap: false,
+                latitude: 42.355244,
+                longitude: 13.393662,
+                depth: 0,
+                magnitude: 0,
+                shakemapName: 'shakemap_1'
+            };
+            $scope.currentParams = {
+                useShakemap: false,
+                latitude: 42.355244,
+                longitude: 13.393662,
+                depth: 0,
+                magnitude: 0,
+                shakemapName: 'shakemap_1'
+            };
             
             $scope.magnitudeTooltip = 'The magnitude of the earthquake';
             $scope.depthTooltip = 'The depth of the earthquake in meters';
+            
+            $scope.params.tdvRounds = 1;
+            // 0 is first step, the tdv round config
+            $scope.currentRound = 0;
+            
+            $scope.beforeNext = function() {
+                if($scope.currentRound > 0) {
+                    $scope.params['eq' + $scope.currentRound] = angular.copy($scope.currentParams);
+                    if($scope.params['eq' + ($scope.currentRound + 1)]) {
+                        $scope.currentParams = angular.copy($scope.params['eq' + ($scope.currentRound + 1)]);
+                    } else {
+                        $scope.currentParams = angular.copy($scope.defaultParams);
+                    }
+                }
+                $scope.currentRound++;
+            };
+            $scope.beforePrevious = function() {
+                if($scope.currentRound > 1) {
+                    $scope.params['eq' + $scope.currentRound] = angular.copy($scope.currentParams);
+                    $scope.currentParams = angular.copy($scope.params['eq' + ($scope.currentRound - 1)]);
+                }
+                $scope.currentRound--;
+            };
+            
+            // TODO: gotoStep impl
+            // gotostep disabled in wizard source
+            // wizard sources adapted to support step removal
+            
+            $scope.$watch('params.tdvRounds', function(n) {
+                // intentional fall-through
+                switch($scope.params.tdvRounds) {
+                    case 1: {
+                        $scope.params.eq2 = undefined;
+                    };
+                    case 2: {
+                        $scope.params.eq3 = undefined;
+                    };
+                    case 3: {
+                        $scope.params.eq4 = undefined;
+                    };
+                    case 4: {
+                        $scope.params.eq5 = undefined;
+                    };
+                }
+            });
             
             epiCenterIcon = L.icon({
                 shadowUrl: null,
@@ -31,7 +99,7 @@ angular.module(
             });
             
             marker = L.marker(
-                [$scope.params.latitude, $scope.params.longitude], 
+                [$scope.currentParams.latitude, $scope.currentParams.longitude], 
                 {
                     icon: epiCenterIcon,
                     clickable: false,
@@ -46,23 +114,32 @@ angular.module(
             };
             
             $scope.createLatLng = function () {
-                setEpicenter([$scope.params.latitude, $scope.params.longitude]);
+                setEpicenter([$scope.currentParams.latitude, $scope.currentParams.longitude]);
             };
             
             leafletData.getMap('eqParamMap').then(function (map) {
                 map.on('click', function (event) {
-                    $scope.params.latitude = event.latlng.lat;
-                    $scope.params.longitude = event.latlng.lng;
+                    $scope.currentParams.latitude = event.latlng.lat;
+                    $scope.currentParams.longitude = event.latlng.lng;
                     setEpicenter(event.latlng);
                 });
 
                 marker.addTo(map);
             });
             
-            $scope.startSimulation = function () {
-                $scope.params.run = true;
-                $scope.$hide();
-            };
+            $scope.$watch('wizard.currentStep', function (n) {
+                if (n) {
+                    if ($scope.wizard.isFinishStep()) {
+                        $scope.wizard.proceedButtonText = 'Run Simulation';
+                    } else {
+                        $scope.wizard.proceedButtonText = 'Next';
+                    }
+                }
+                
+                leafletData.getMap('eqParamMap' + $scope.currentRound).then(function (map) {
+                    map.invalidateSize(false);
+                });
+            });
         }
     ]
 );
